72d98b665e570e384abbfc2f1c63ca640d5f0908
w4ndry <eriwandri@gmail.com>
2017-09-28 00:49:53 +0700

    first configuration

---
+++ gulpfile.babel.js
@@ -0,0 +1,43 @@
+import hmr from 'browserify-hmr';
+import watchify from 'watchify';
+import gulp from "gulp";
+import browserify from "browserify";
+import source from "vinyl-source-stream";
+import sass from "gulp-sass";
+
+gulp.task('default', () => {
+    return browserify({
+            entries: 'resources/assets/js/app.js',
+            debug: true
+        })
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('hmr', () => {
+    const b = browserify({
+        entries: entry,
+        plugin: [hmr, watchify],
+        debug: true
+    })
+
+    b.on('update', bundle);
+    bundle();
+
+    function bundle() {
+        b.bundle()
+            .on('error', err => {
+                util.log("Browserify Error", util.colors.red(err.message))
+            })
+            .pipe(source(entry))
+            .pipe(flatten())
+            .pipe(gulp.dest(dest));
+    }
+});
+
+gulp.task('sass', function() {
+    return gulp.src('./resources/assets/sass/app.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css'));
+});
---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="ui center aligned padded grid">
+    <div class="column">
+      <div class="ui segment">
+        <h1>Setup Complete guys!</h1>
+      </div>
+    </div>
+  </div>
+</template>
